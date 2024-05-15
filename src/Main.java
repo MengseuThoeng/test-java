12675e2940f38ae9d1e8186a66d439ff32a585d1
MengseuThoeng <mengseu2004@gmail.com>
2024-05-15 09:47:13 +0700

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static String output; // Field to store output for testing
+    private static String output;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        if(args.length > 0) {
+            // get the input from argument if present
+            int num = Integer.parseInt(args[0]);
+            String result = (num % 2 == 0) ? "Even" : "Odd";
 
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
+            output = "The number " + num + " is " + result;
+            System.out.println(output);
+        } else {
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
 
-        String result = (num % 2 == 0) ? "Even" : "Odd";
+            String result = (num % 2 == 0) ? "Even" : "Odd";
 
-        output = "The number " + num + " is " + result;
-        System.out.println(output);
+            output = "The number " + num + " is " + result;
+            System.out.println(output);
+        }
+        // Remember to close the scanner
+        scanner.close();
+
     }
-
-    // Getter method for output (needed for testing)
     public static String getOutput() {
         return output;
     }
