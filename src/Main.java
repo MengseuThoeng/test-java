6b4271a5c67b6fba94d9ebb64d8f6420bf2f6605
MengseuThoeng <mengseu2004@gmail.com>
2024-05-15 10:31:08 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     private static String output;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {  // Added "throws InputMismatchException"
         Scanner scanner = new Scanner(System.in);
         int num;
-        try {
-            if (args.length > 0) {
-                num = Integer.parseInt(args[0]);
-            } else {
-                System.out.print("Enter a number: ");
+
+        if (args.length > 0) {
+            num = Integer.parseInt(args[0]);
+        } else {
+            System.out.print("Enter a number: ");
+            try{
                 num = scanner.nextInt();
             }
-
-            String result = (num % 2 == 0) ? "Even" : "Odd";
-            output = "The number " + num + " is " + result;
-        } catch (NumberFormatException e) {
-            output = "Invalid input. Please enter a number.";
+            catch(InputMismatchException e){
+                //If the input is not a valid number throw an InputMismatchException
+                throw new InputMismatchException("Invalid input. Please enter a number.");
+            }
         }
+
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        output = "The number " + num + " is " + result;
         System.out.println(output);
         scanner.close();
-
     }
+
     public static String getOutput() {
         return output;
     }
